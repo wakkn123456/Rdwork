1f452e400ca9c96975d23a254c68731e73c73e8c
wakkn123 <wakkn12@gmail.com>
2017-11-25 09:57:25 +0800

    todolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
