d9b5df3ac657c2f3b93303d558cee4ed3d8d24f4
wakkn123 <wakkn12@gmail.com>
2017-11-25 10:25:11 +0800

    任務 06：套用事件繫結 (Event Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  inputHint = '請輸入你要的文字';
+
+  onEnter = function(value){
+    var todos:string[];
+    //console.info(value);
+    todos.push(value);
+    console.info(todos);
+  }
 }
